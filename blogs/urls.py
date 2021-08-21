047b3c3a3671b3232566881b986182d746b3833b
uwizeyimanaphilippe <philippe.uwizeye@gmail.com>
2021-08-21 21:03:48 +0200

    Starting & configuring Blogs app, creating Post model, updating team section and static urlpatterns

---
+++ blogs/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .import views
+
+app_name='blogs'
+
+urlpatterns = [
+    path('', views.blog_post, name='blog_home'),
+    path('<str:slug>', views.post_detail, name='detail'),
+    # path('search-post', views.search_post, name='search')
+]
