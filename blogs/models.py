1c48580b9bddb9ceace6da7cec762f2bdc3b0558
nyakamwe <nyakamweaimable@gmail.com>
2021-08-23 14:59:02 +0200

    adding Thumb fields, and updating requirements.txt

--- blogs/models.py
+++ blogs/models.py
@@ -9,6 +9,7 @@ class Post(models.Model):
     body = models.TextField()
     slug = models.SlugField()
     date = models.DateTimeField(auto_now_add=True)
+    thumb = models.ImageField(default='default.jpg', blank=True)
 
     def __str__(self):
         return self.title
