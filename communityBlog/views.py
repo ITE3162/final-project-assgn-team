d1047fa4336ac309b257911e4075eb8a1737b6f4
nyakamwe <nyakamweaimable@gmail.com>
2021-08-30 22:16:41 +0200

    Making about dynamic

--- communityBlog/views.py
+++ communityBlog/views.py
@@ -1,9 +1,10 @@
 from django.core.mail import send_mail
 from django.shortcuts import render
 from blogs.models import Post
-
+from about.models import AboutUs
 
 def homepage(request):
     blogs = Post.objects.all().order_by("title")
-    return render(request, 'homepage.html', {'blogs': blogs})
+    about = AboutUs.objects.all()
+    return render(request, 'homepage.html', {'blogs': blogs, 'aboutus':about})
 
--- contacts/models.py
+++ contacts/models.py
@@ -9,4 +9,4 @@ class ContactInfo(models.Model):
     contacted_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return   self.subject
+        return self.subject
