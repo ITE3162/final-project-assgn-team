57186f12a9ac02a9ddb21cb25bb52a56929a095d
uwizeyimanaphilippe <philippe.uwizeye@gmail.com>
2021-09-12 22:34:58 +0200

    updated settings

--- communityBlog/settings.py
+++ communityBlog/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = str(os.getenv('SECRET_KEY'))
-
+# SECRET_KEY = str(os.getenv('SECRET_KEY'))
+SECRET_KEY="1a349df94025dd9415ea4e633261bb96873f05561d2163ce"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cinemamiacommunity.herokuapp.com']
 
 
 # Application definition
